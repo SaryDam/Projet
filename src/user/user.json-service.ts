c0e41f57cd39efa3d6e798c9c4097cbd31d95c52
dcreux <d.creux@groupe-burus.tech>
2023-09-20 15:09:02 +0200

    ajout de fonction add / getById

--- src/user/user.json-service.ts
+++ src/user/user.json-service.ts
@@ -1,12 +1,56 @@
+import { readFile, writeFile } from 'fs';
 import { user } from './user';
 import { UserService } from './user.service';
 
 export class UserJSONService implements UserService {
+
+    private newUser! : user
+    private data! : string
+
     add(username: string): user {
-        throw new Error('Method not implemented.');
+        readFile('./src/user/utilisateurJson.json', 'utf8', (err, data) => {
+            if (err){
+                console.error('erreur de lecture')
+            }
+            this.data = data;
+        }) 
+        
+            let utilisateur = JSON.parse(this.data);
+            const Id = this.genererId()
+            
+            this.newUser = new user(Id, username);
+            console.log(this.newUser)
+            utilisateur.push(this.newUser);
+            
+            const addUser = JSON.stringify(utilisateur);
+
+            writeFile('./src/user/utilisateurJson.json', addUser, 'utf8', (err) => {
+                if (err) {
+                    throw new Error('erreur lors de l\'ecriture du fichier');
+                }
+            })
+        return this.newUser
+    }
+
+    genererId() : number {
+        return Math.floor(Math.random() * 999999);
     }
 
     getById(id: number): user | null {
-        throw new Error('Method not implemented.');
+        readFile('./src/user/utilisateurJson.json', 'utf8', (err, data) => {
+            if (err){
+                console.error('erreur de lecture')
+            }
+            this.data = data;
+            console.log("11111111111111111111111111");
+        }) 
+        const dataJson = JSON.parse(this.data);
+        console.log("2222222222222222222222");
+        for(let i = 0; i < dataJson.length; i++){
+            if (dataJson[i].id == id){
+                this.newUser = dataJson[i]
+            }
+        }
+    return this.newUser;
     }
 }
